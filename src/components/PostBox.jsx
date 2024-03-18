2bab884f90e90159b56cced1636e1d4a53822990
Shin Sangwoo <bano112@naver.com>
2024-03-18 19:38:54 +0900

    pagenatino UI 완성

--- src/components/PostBox.jsx
+++ src/components/PostBox.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PostItem from './PostItem';
 import usePosts from './hooks/usePosts';
 import Loading from './Loading';
+import Pagination from 'react-js-pagination';
+import '../styles/pagination.css';
 
 const PostBox = () => {
+  const [page, setPage] = useState(1);
+
   const {
     postQuery: { isLoading, error, data: postsData },
   } = usePosts();
+
+  const handlePageChange = (page) => {
+    setPage(page);
+  };
 
   return (
     <article className="mb-20">
@@ -16,6 +24,17 @@ const PostBox = () => {
         postsData
           .map((data) => <PostItem key={data.id} post={data} />)
           .reverse()}
+      <Pagination
+        activePage={page} // 현재 페이지
+        itemsCountPerPage={5} // 한 페이지에서 보여줄 post 개수
+        totalItemsCount={300} // 총 post 개수
+        pageRangeDisplayed={5} // paginator의 페이지 범위
+        prevPageText={'<'}
+        nextPageText={'>'}
+        firstPageText="<<"
+        lastPageText=">>"
+        onChange={handlePageChange}
+      ></Pagination>
     </article>
   );
 };
