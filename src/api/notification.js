bfc98e9412ede95c372d647d72fdea1bd8dc2fa7
Shin Sangwoo <bano112@naver.com>
2024-03-27 21:03:13 +0900

    Notification api 작성

---
+++ src/api/notification.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import { BASE_URL, accessToken } from '../components/config/data';
+
+// 받음 알림 조회
+export const inQuiryNotification = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/notification`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// 알림 구독
+export const subscribeNotification = async () => {
+  try {
+    await axios.get(`${BASE_URL}/subscribe`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// 알림 확인
+export const checkedNotification = async (notificationId) => {
+  try {
+    await axios.post(
+      `${BASE_URL}/${notificationId}`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
