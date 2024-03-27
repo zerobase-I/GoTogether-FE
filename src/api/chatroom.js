d1891e03b549d0845ff417d84a96310f7033bad2
Luison1472 <melody9908@naver.com>
2024-03-27 19:56:11 +0900

    ChatRoom 작업중

--- src/api/chatroom.js
+++ src/api/chatroom.js
@@ -45,22 +45,6 @@ export const getChatRoomLists = async () => {
   }
 };
 
-//채팅방 입장을 처리하는 함수
-export const enterChatRoom = async (accessToken, chatRoomId) => {
-  try {
-    
-    apiClient.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
-    
-    const response = await apiClient.post(`chat-room/enter/${chatRoomId}`, {
-   
-    });
-    console.log(response.data);
-    return response.data;// 서버로부터 받은 응답 데이터를 반환합니다.
-  } catch (error) {
-    console.error('Chat room entry failed:', error);
-    throw error;
-  }
-};
 
      
 // 채팅방 퇴장을 처리하는 함수
