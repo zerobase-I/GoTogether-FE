2a1daeeb8a7865beb21b4296fb50faa7bffd25cc
Luison1472 <melody9908@naver.com>
2024-03-28 01:42:24 +0900

    채팅방 구현완료

--- src/components/Layouts/chatRoomHeader.jsx
+++ src/components/Layouts/chatRoomHeader.jsx
@@ -6,10 +6,10 @@ const ChatRoomHeader = () => {
   const { roomName } = location.state || {}; // 상태가 없는 경우를 위해 기본값 설정
 
   return (
-      <header className="flex justify-between items-center h-20 bg-gray-800 px-4 sm:px-6 lg:px-8 shadow-md sticky top-0 z-10">
+      <header className="flex justify-between items-center h-20 bg-gray-800 pl-8 pr-2 sm:px-6 lg:px-8 shadow-md sticky top-0 z-10">
           <img src="/src/assets/right-arrow-white.png" className="w-6 rotate-180"/>
-      <h1 className="text-xl text-white">{roomName}</h1>
-      {/* 나머지 코드 */}
+      <h1 className="text-xl pl-6 text-white">{roomName}</h1>
+      <div className="text-transparent bg-transparent"><h1>토글버튼</h1></div>
     </header>
   );
 };
